2f8808341bb1d2eb0000735de3589240c874417c
MatthijsKock <97100416@st.deltion.nl>
2025-12-11 10:48:06 +0100

    Afronding opdracht 2

--- opdrachten/010_variabelen/opdr_2/opdr_2.py
+++ opdrachten/010_variabelen/opdr_2/opdr_2.py
@@ -1,7 +1,9 @@
 # Opdracht 1
-# Naam student:
-# Groep:
+# Naam student: Matthijs Kock
+# Groep: IT-Flex
 
 # Hier komt je code...
 
-my_tuple = ...
+my_tuple = (11, "oh een tuple", 13.5, 8009)
+
+print(my_tuple)
