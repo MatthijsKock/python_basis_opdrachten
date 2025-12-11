8f6c4c330f07c4fc77bf8f8efd989333daaf2a52
MatthijsKock <97100416@st.deltion.nl>
2025-12-11 13:29:52 +0100

    Afronding opdracht 2 020

---
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2_020.py
@@ -0,0 +1,13 @@
+# Opdracht 1
+# Naam student: Matthijs Kock
+# Groep: IT-Flex
+
+# Hier komt je code...
+
+my_string = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"
+
+print(my_string)
+
+my_list = list(my_string)
+
+print(my_list.count("t"))
