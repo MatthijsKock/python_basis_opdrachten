41de1696eddf1fdd80f236470a4a75b476e4d0cd
MatthijsKock <97100416@st.deltion.nl>
2025-12-11 13:42:10 +0100

    Afronding opdracht 3 020

---
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3_020.py
@@ -0,0 +1,15 @@
+# Opdracht 3 tekstfuncties
+# Naam student:
+# Groep:
+
+# Hier komt je code...
+
+print(("    *       ") * 5)
+print(("   ***      ") * 5)
+print(("  ******    ") * 5)
+print((" ********   ") * 5)
+print(("*********** ") * 5)
+print(("    ***     ") * 5)
+print(("    ***     ") * 5)
+print(("    ***     ") * 5)
+
